4c759537b9ced8ddf9285b1ac2dd46103731fe10
DESKTOP-SMCA <sommacalalberto@gmail.com>
2024-07-22 22:10:35 +0200

    finish project

--- server/index.js
+++ server/index.js
@@ -47,3 +47,19 @@ mongoose
     // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
+
+
+  
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri = process.env.MONGO_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
